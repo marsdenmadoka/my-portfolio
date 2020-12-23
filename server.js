16f36b56fbf953f57dff9b848ea7661f6cacfe86
marsdenmadoka <marsmadoka98@gmail.com>
2020-12-23 23:19:46 +0300

    my portfolio

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.set('views', __dirname + '/views');
 app = config(app);
 
 
-mongoose.connect("mongodb://localhost/imgPloadr",{useNewUrlParser: true,useCreateIndex:true,useUnifiedTopology: true}); 
-mongoose.connection.on('open', function() {
-console.log('Mongoose connected.');
-});
+// mongoose.connect("mongodb://localhost/",{useNewUrlParser: true,useCreateIndex:true,useUnifiedTopology: true}); 
+// mongoose.connection.on('open', function() {
+// console.log('Mongoose connected.');
+// });
 
 app.listen(app.get('port'), function() {
     console.log('Server up: http://localhost:' + app.get('port'));
